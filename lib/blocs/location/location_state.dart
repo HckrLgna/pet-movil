ffe00f97e22f335f2d571c2d86b1be0a1e4cfd08
jake123426 <xxjacques1234@gmail.com>
2022-12-29 00:22:35 -0400

    Creado gps y location bloc

---
+++ lib/blocs/location/location_state.dart
@@ -0,0 +1,28 @@
+part of 'location_bloc.dart';
+
+class LocationState extends Equatable {
+
+  final bool followingUser;
+  final LatLng? lastKnownLocation;
+  
+
+  const LocationState({
+    this.followingUser = false,
+    this.lastKnownLocation,
+    myLocationHistory    
+  });
+
+  LocationState copyWith({
+    bool? followingUser,
+    LatLng? lastKnownLocation,
+    List<LatLng>? myLocationHistory,
+  }) => LocationState(
+    followingUser    : followingUser ?? this.followingUser,
+    lastKnownLocation: lastKnownLocation ?? this.lastKnownLocation,    
+  );
+  
+  @override
+  List<Object?> get props => [ followingUser, lastKnownLocation ];
+}
+
+
